32da416fea5e95d46a429bf918de27dd32eb2a3a
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-12-27 18:14:30 +0100

    Initial commit

---
+++ src/main/java/pl/kukla/krzys/bluetrade/btstockreceiver/StockReceiverApplication.java
@@ -0,0 +1,13 @@
+package pl.kukla.krzys.bluetrade.btstockreceiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockReceiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockReceiverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kukla/krzys/bluetrade/btstockreceiver/StockReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kukla.krzys.bluetrade.btstockreceiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockReceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
